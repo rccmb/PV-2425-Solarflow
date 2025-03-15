7e27d29cd0933a28bc1f010372f434c48976a106
afonm <matos.afonsofilipe@gmail.com>
2025-03-15 19:33:12 +0000

    Create Battery.cs

---
+++ SolarflowSource/SolarflowServer/Models/Battery.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolarflowServer.Models
+{
+    public class Battery
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public int Value { get; set; }
+
+        public int? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
