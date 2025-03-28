e149056eab728c0e6b130ef40405ae9c541b26d8
hrodric-baptista <rodrigobaptistarodrigo@gmail.com>
2025-03-28 11:12:39 +0000

    CI/CD Test with database update.

---
+++ SolarflowSource/SolarflowServer/Migrations/20250328110459_BaselineMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarflowServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class BaselineMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
