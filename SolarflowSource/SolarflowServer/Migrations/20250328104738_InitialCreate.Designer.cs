5e8e3382a684f2bc3b18f093cb124ba16652fc59
hrodric-baptista <rodrigobaptistarodrigo@gmail.com>
2025-03-28 10:48:10 +0000

    CI/CD Test with database update.

--- SolarflowSource/SolarflowServer/Migrations/20250328104738_InitialCreate.Designer.cs
+++ SolarflowSource/SolarflowServer/Migrations/20250328104738_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace SolarflowServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250327135125_InitialCreate")]
+    [Migration("20250328104738_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
